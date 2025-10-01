0222edea69373953912ce842b33ab5f53fe8ed00
dragonwhite <dragonwhiteatom@gmail.com>
2025-10-01 15:43:06 +0700

    dfhdfhdh

---
+++ Assets/GI479Assets/Week4/Ours/Scripts/Bullet.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    public float bulletDamage = 10.5f;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float speed = 10f;
+
+    [SerializeField]
+    private float autoDestroyTime = 15f;
+
+    [SerializeField]
+    private GameObject hitParticle;
+
+    private void OnValidate()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        Destroy(gameObject, 15f);
+        rb.linearVelocity = speed * transform.forward;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (hitParticle != null && collision.contactCount > 0)
+        {
+            var contact = collision.GetContact(0);
+            var hitPos = contact.point;
+            var hitRot = Quaternion.LookRotation(contact.normal);
+
+            Instantiate(hitParticle, hitPos, hitRot);
+        }
+        if (collision.gameObject.TryGetComponent(out EnemyHealth health))
+        {
+            health.TakeDamage(bulletDamage);
+        }
+
+        Destroy(gameObject);
+    }
+}
